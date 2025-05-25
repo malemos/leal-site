5db4dda5e659e60261db905dc001df7343f707e4
Marcos Lemos <vte_mlemos@uolinc.com>
2025-05-25 18:35:49 -0300

    Fix CPF/CNPJ mask regex

--- js/cadastro.js
+++ js/cadastro.js
@@ -37,7 +37,7 @@ function aplicarMascaraCpfCnpj(input) {
       valor = valor.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
     } else {
       valor = valor.replace(/^(\d{2})(\d)/, "$1.$2");
-      valor = valor.replace(/^(\\d{2})\.(\\d{3})(\\d)/, "$1.$2.$3");
+      valor = valor.replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3");
       valor = valor.replace(/\.(\d{3})(\d)/, ".$1/$2");
       valor = valor.replace(/(\d{4})(\d)/, "$1-$2");
     }
